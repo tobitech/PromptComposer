867f9bb71fd380e169a8733af1b06c57cb5c3ad5
Tobi Omotayo <tobitech@ymail.com>
2026-02-04 15:05:35 +0100

    Initial Commit

---
+++ Composer/ComposerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposerApp.swift
+//  Composer
+//
+//  Created by Oluwatobi Omotayo on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ComposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Composer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Composer
+//
+//  Created by Oluwatobi Omotayo on 04/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
